75e233c46355bbd2cc7555d9d28a08d8b23f7620
darkvas <darkvas90@gmail.com>
2015-08-24 09:46:54 +0400

    prepared code to test crm - added complain

--- test/testHandlers/traServicesCrm.js
+++ test/testHandlers/traServicesCrm.js
@@ -2,7 +2,6 @@
 
 var request = require('supertest');
 var expect = require('chai').expect;
-var mongoose = require('mongoose');
 var CONST = require('../../constants/index');
 var USERS = require('./../testHelpers/usersTemplates');
 var SERVICES = require('./../testHelpers/servicesTemplates');
@@ -10,8 +9,8 @@ var async =  require('async');
 var PreparingDB = require('./preparingDB');
 var url = 'http://localhost:7791';
 
-describe('TRA Services tests  CRM', function () {
-    this.timeout(20000);
+describe('TRA CRM Services tests SMSSpam', function () {
+    this.timeout(30000);
 
     var agent = request.agent(url);
     var serviceCollection;
@@ -22,33 +21,47 @@ describe('TRA Services tests  CRM', function () {
         var preparingDb = new PreparingDB();
 
         async.series([
-                preparingDb.dropCollection(CONST.MODELS.USER + 's'),
-                preparingDb.dropCollection(CONST.MODELS.SERVICE + 's'),
                 preparingDb.toFillUsers(1),
-                preparingDb.createUsersByTemplate(USERS.CLIENT),
-                preparingDb.createUsersByTemplate(USERS.COMPANY),
-                preparingDb.createServiceByTemplate(SERVICES.SERVICE_CHECK_DOMAIN_AVAILABILITY_TMA_TRA_SERVICES),
-                preparingDb.createServiceByTemplate(SERVICES.SERVICE_CAPALABA_RITEILS)
+                preparingDb.createUsersByTemplate(USERS.CLIENT)
             ],
             function (err, results) {
                 if (err) {
                     return done(err)
                 }
+
                 done();
             });
     });
 
-    it('GET CRM CASES', function (done) {
+    it('SEND data to ComplainSmsSpam', function (done) {
+
+        var loginData = USERS.CLIENT_CRM_LOGIN_DATA;
+        var data = {
+            phone: '505050440',
+            description: 'I receive 10 sms from this number 505050440'
+        };
 
         agent
-            .get('/crmwrapper/case')
+            .post('/crm/signIn')
+            .send(loginData)
             .expect(200)
             .end(function (err, res) {
                 if (err) {
                     return done(err)
                 }
-                console.dir(res.body);
-                done();
+
+                agent
+                    .post('/crm/complainSmsSpam')
+                    .send(data)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err)
+                        }
+                        console.dir(res.body);
+                        done();
+                    });
             });
     });
+
 });
